91cf7e23447505bbf56b6eed151f0a7028113aad
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-03-11 01:38:56 -0700

    continue solve one question
    continue

---
+++ chapter-1/script.js
@@ -0,0 +1,22 @@
+// question 1
+alert("welcome to visit your website");
+// question 1
+
+// question 2
+document.write("hello how can I help you");
+// question 2
+
+// question 3
+alert("Welcome to Js landing \n Happy coding!");
+// question 3
+
+// question 4
+// question 4
+
+// question 5
+alert("Hello I can run JS through my web browser's console");
+// question 5
+
+ // question 6 
+ 
+ // question 6
