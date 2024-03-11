6dcfc24f2fbd9cb7b0066833aa0a40ca29377502
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-03-11 02:15:58 -0700

    complete chapter 2
    complete

--- chapter-2/app.js
+++ chapter-2/app.js
@@ -1,7 +1,47 @@
 // question 1
-
+var username;
 // question 1
 
 // question 2
-
+var fullname = "M.D Faizan Ali";
 // question 2
+
+// question 3
+var message;
+var message = "Hello world";
+alert(message);
+// question 3
+
+// question 4
+var student = "MD Faizan Ali";
+alert(student);
+var student = 15;
+alert(student);
+var student = "Certified Mobile Application Development";
+alert(student);
+// question 4
+
+// question 5
+var pizza = "PIZZA /np PIZZ /np PIZ /np PI /np P";
+alert(pizza);
+// question 5
+
+// question 6
+var email = "example@example.com";
+alert(email);
+// question 6
+
+// question 7
+var book = "A smarter way to learn JavaScript";
+alert(book);
+// question 7
+
+// question 8
+document.write("Yah! I can write HTML content through Javascript");
+// question 8
+
+// question 9
+var hi = "how can I help you";
+alert(hi);
+document.write(hi);
+// question 9
