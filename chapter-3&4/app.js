02b384cc4bf795e89d8b11849dd4040cd09d0ca5
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-03-11 02:35:18 -0700

    complete chap 3-4
    complete

---
+++ chapter-3&4/app.js
@@ -0,0 +1,20 @@
+    // chapter 3 solve
+var age = "Hello I'm 18";
+alert(age);
+
+var site = "You have visited this site 100 times<br>";
+alert(site);
+
+var birth = "Born in 2004 finally /np<br>";
+document.write(birth);
+
+var customer = "Mr lee order some Tshirts on FA clothing /n";
+document.write(customer);
+    // chapter 3 solve   
+
+    // chapter 4 solve
+    document.write("<h1>Rules For Naming JS Variable\n</h1>")
+    document.write("a variable can only contain letter,number Dollarsign and underscore For example $my_1stVariable\n<br>")
+    document.write("Variables names are case sensitive \n<br>" )
+    document.write("Variables Names shpuld not be JS keywords \n")
+    // chapter 4 solve
