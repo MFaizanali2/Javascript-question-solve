4d80fff29bd17d09cc4624de12ddf002447c897c
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-03-12 19:51:02 -0700

    stell working
    continue

---
+++ chapter-5-9/app.js
@@ -0,0 +1,89 @@
+    // chapter 5
+
+// question-1
+var a = 3;
+var b = 5;
+document.write("The Sum of 3 and 5 is ="," ",a + b, "<br><br>");
+// question-1
+
+// question-2
+var a = 5;
+var b = 3;
+document.write("The Substract of 5 and 3 is ="," ",a - b, "<br><br>");
+
+var a = 5;
+var b = 3;
+document.write("The multiply of 5 and 3 is ="," ",a * b, "<br><br>");
+
+var a = 225;
+var b = 5;
+document.write("The divide of 5 and 225 is ="," ",a / b, "<br><br>");
+// question-2
+
+// question-3
+var a = 10;
+var first ="how can I help you";
+document.write(first, "<br><br>");
+var second = "Value after variable declaration is: ??";
+document.write(second, "<br><br>");
+var intial = "Initial value is 5";
+document.write(intial, "<br><br>");
+var increment = "value after increment is 5";
+document.write(increment, "<br><br>");
+var addition = "value after addition is 13";
+document.write(addition, "<br><br>");
+var decrement = "value after decrement is 12";
+document.write(decrement, "<br><br>");
+var output = "The reminder is 0";
+document.write(output, "<br><br>");
+document.write(a + 7, "<br><br>");
+// question-3
+
+// question-4
+var cost = 600;
+document.write("Total cost to buy 5 Tickets to a movie is"," ", cost*5,"Pkr","<br><br>");
+// question-4
+
+// question-5
+var table = 4;
+document.write("4 x 1 ="," ",table*1,"<br><br>")
+document.write("4 x 2 ="," ",table*2,"<br><br>")
+document.write("4 x 3 ="," ",table*3,"<br><br>")
+document.write("4 x 4 ="," ",table*4,"<br><br>")
+document.write("4 x 5 ="," ",table*5,"<br><br>")
+document.write("4 x 6 ="," ",table*6,"<br><br>")
+document.write("4 x 7 ="," ",table*7,"<br><br>")
+document.write("4 x 8 ="," ",table*8,"<br><br>")
+document.write("4 x 9 ="," ",table*9,"<br><br>")
+document.write("4 x 10 ="," ",table*10,"<br><br>")
+// question-5
+
+// question-6
+var shopping = "<h1>Shopping Cart</h1>";
+document.write(shopping, "<br><br><br>");
+var price1 = 650;
+document.write("Price of Item 1 is"," ",price1,"<br><br>");
+var quantity1 = 3;
+document.write("quantity of Item 1 is"," ",quantity1,"<br><br>");
+var price2 = 100;
+document.write("Price of Item 2 is"," ",price2,"<br><br>");
+var quantity2 = 7;
+document.write("quantity of Item 2 is"," ",quantity2,"<br><br>");
+var charges = 100;
+document.write("Shipping charges","  ",charges,"<br><br><br>");
+var total = price1*quantity1+price2*quantity2+charges;
+document.write("Total cost of your item is"," ",total,"<br><br>");
+// question-6
+
+// question-7
+var mark = "<h1>Marksheet</h1>";
+document.write(mark, "<br><br><br>");
+var total_mark = 980;
+document.write("Total marks is"," ", total_mark,"<br><br>");
+var obtained = 804;
+document.write("obtained marks is"," ", obtained,"<br><br>");
+var per = 804/100*10;
+document.write("Percentage ="," ",per);
+// question-7
+
+    // chapter 5
