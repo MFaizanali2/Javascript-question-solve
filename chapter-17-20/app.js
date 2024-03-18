ac77c8da5f7b2ed474be75c607d0d5c9707ecf80
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-03-17 23:19:24 -0700

    continuing working
    continue

---
+++ chapter-17-20/app.js
@@ -0,0 +1,33 @@
+// question 1
+let multidimArray = [];
+multidimArray.push([]);
+multidimArray.push([]);
+// question 1
+
+// question 2
+var multi = [0];
+multi.push([1,2,3],"<br>");
+multi.push([1,0,1,2],"<br>");
+multi.push([2,1,0,1],"<br>");
+// multi.push([2]);
+// multi.push([3]);
+document.write(multi);
+// question 2
+
+// question 3
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+// question 3
+
+// question 4
+
+// question 4
+
+// question 5
+
+// question 5
+
+// question 6
+
+// question 6
